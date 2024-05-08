3df99c3a0891f6539df7ebb38a29f85fe13b13a0
Jeanmuribia <jean.muribia@gmail.com>
2024-05-08 12:59:07 +0200

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBPEpTNNO566FVsORTVi4T0BmL9NesGWQE",
+  authDomain: "snapplot-6a1ff.firebaseapp.com",
+  projectId: "snapplot-6a1ff",
+  storageBucket: "snapplot-6a1ff.appspot.com",
+  messagingSenderId: "85638164388",
+  appId: "1:85638164388:web:70573ccec7c8d276d3db31",
+  measurementId: "G-LTLMT2FG1C"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
---
+++ src/userComponents/Signup.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import firebase from './firebase';
+
+const Signup = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignup = async () => {
+    try {
+      await firebase.auth().createUserWithEmailAndPassword(email, password);
+      // Navigate to the dashboard or do something else upon successful signup
+    } catch (error) {
+      console.error('Error signing up:', error.message);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Signup</h2>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleSignup}>Signup</button>
+    </div>
+  );
+};
+
+export default Signup;
